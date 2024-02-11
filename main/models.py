29e335c2327cff21daf622240e855a6335c062a0
Mr.Ramzeng <mr.ramzeng@gmail.com>
2024-02-11 17:09:48 +0300

    added length delimiter

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
 
 # Create your models here.
@@ -13,4 +14,29 @@ class Book(models.Model):
         verbose_name = 'книга'
 
     def __str__(self):
-        return self.name
+        return self.name[:511]
+
+    def html_text(self):
+        text = self.text
+        index = 0
+        space_idx = 0
+        tags = []
+        # todo: edit
+        for tag in list(text):
+            if tag == '\r':
+                tags.append('<span>&para;\n</span>')
+                index = 0
+            elif tag == '\n':
+                continue
+            elif tag == ' ':
+                tags.append(f'<span>{tag}</span>')
+                if index < 99:
+                    space_idx = len(tags) - 1
+                    index += 1
+                else:
+                    tags[space_idx] = f'<span>{tag}<br></span>'
+                    index = len(tags[space_idx:])
+            else:
+                tags.append(f'<span>{tag}</span>')
+                index += 1
+        return mark_safe(''.join(tags))

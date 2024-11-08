832d83f2f12ce31cb37dc1dd29657f2eff91f78d
mr_ramzeng <mr.ramzeng@gmail.com>
2024-11-08 10:19:30 +0300

    edit sidebar styles

--- main/docx_parser.py
+++ main/docx_parser.py
@@ -62,7 +62,7 @@ def extract_word_data(file_path):
         elif style_name == 'footnote text':
             data.append(
                 f'''<div id="footnote_{footnote_counter}" class="footnote fixed top-0 left-0 
-                right-0 z-50 
+                right-0 z-50 hidden
                 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] 
                     max-h-full">
                     <div class="relative w-full max-w-md max-h-full">
